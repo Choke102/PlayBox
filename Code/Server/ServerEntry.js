d2bb15ca448e0be9ce4f23d610176f758517a70b
Choke102 <git_daniel@outlook.com>
2014-07-12 16:53:58 +1000

    moved client into public under server
    moved client into public under server so could more easily serve up
    static files.

--- Code/Server/ServerEntry.js
+++ Code/Server/ServerEntry.js
@@ -4,8 +4,11 @@ var express = require('express');
  
 var server = express();
 server.use(express.static(__dirname + '/public'));
- 
+
 var port = 5002;
 server.listen(port, function() {
     console.log('server listening on port ' + port);
 });
+
+
+
