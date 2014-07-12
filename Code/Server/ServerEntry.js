3e128edefc341a3576ad68391f618f2d315ef11e
Choke102 <git_daniel@outlook.com>
2014-07-12 15:10:19 +1000

    Created Node ServerEntry shell
    Created Node ServerEntry shell

---
+++ Code/Server/ServerEntry.js
@@ -0,0 +1,11 @@
+
+
+var express = require('express');
+ 
+var server = express();
+server.use(express.static(__dirname + '/public'));
+ 
+var port = 5002;
+server.listen(port, function() {
+    console.log('server listening on port ' + port);
+});
